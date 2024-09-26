bc8b1d151039f9bfff43055fc600bb1e136dd6e2
baterlo <batterlo@hotmail.com>
2024-09-25 20:54:54 -0600

    version tabla1.0 completa

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-var veneno = 10;
+var veneno = 0;
 var vida = 400;
-var daño = veneno * 5 + 50;
+var daño = 0;
 
-document.getElementById('veneno').innerHTML = "Veneno = " +veneno;
-document.getElementById('vida').innerHTML = "Vida = " +vida;
+actualizar();
 
-document.getElementById('daño').innerHTML = daño;
-document.getElementById('doubt').innerHTML = daño*1.25;
-document.getElementById('whisp').innerHTML = daño -50;
-document.getElementById('noWhisp').innerHTML = daño - 100;
-document.getElementById('doubtWhisp').innerHTML = daño*1.25 -50;
-document.getElementById('noDWhisp').innerHTML = daño*1.25 -90;
+function actualizar(){
+  var test = document.getElementById('veneno').value;
+  if (test >=0 && test <=40){
+    veneno = test;
+    daño = veneno * 5 + 50;
+  }
+  test = document.getElementById('vida').value;
+  if (test>0 ){
+    vida = test;
+  }
+  document.getElementById('daño').innerHTML = daño;
+  document.getElementById('vida1').innerHTML = vida-daño;
+  document.getElementById('doubt').innerHTML = daño*1.25;
+  document.getElementById('vida2').innerHTML = vida-daño*1.25;
+  document.getElementById('whisp').innerHTML = daño -50;
+  document.getElementById('vida3').innerHTML = vida-(daño -50);
+  document.getElementById('noWhisp').innerHTML = daño - 100;
+  document.getElementById('vida4').innerHTML = vida-(daño - 100);
+  document.getElementById('doubtWhisp').innerHTML = daño*1.25 -50;
+  document.getElementById('vida5').innerHTML = vida-(daño*1.25 -50);
+  document.getElementById('noDWhisp').innerHTML = daño*1.25 -90;
+  document.getElementById('vida6').innerHTML = vida-(daño*1.25 -90);
+}
+
+document.getElementById('btn1').addEventListener('click',actualizar);
+
